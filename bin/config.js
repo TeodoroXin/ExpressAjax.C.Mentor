bc1c9f0f9acdf09cfe2c9dd05fbccf8e89fb103b
TeodoroXin <71947981+TeodoroXin@users.noreply.github.com>
2023-05-10 11:53:21 -0400

    Add files via upload

---
+++ bin/config.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const configurationOBJ = {
+  rootDir : '#'
+  , siteMapOBJ : {
+    'p1':'/proyecto1.html', 'p2':'/proyecto2.html', '02':'/02-devolver-texto.html',
+    '03':'/03-json-array.html', '04':'/04-nested-json.html', '05':'/05-encontrando-menor.html', '06':'/06-encontrar-persona.html',
+    '07':'/07-devolver-nombre.html', '08':'/08-calculadora.html', '09':'/09-sugerencia-nombre.html', '10':'/10-conexion-mysql.html',
+    '11':'/11-seleccionar-usuario.html', '12':'/12-buscador-sugerencias.html', '13': '13-insert-express-mysql.html', '14': '/14-leer-xml.html',
+    '15': '/15-leer2-xml.html', '16': '16-select-nombres-xml.html', '17': '17-buscador-usuario-xml.html'
+  }
+}
+
+module.exports = configurationOBJ;
